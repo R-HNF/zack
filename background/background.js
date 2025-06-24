065357c6a5643d23970befaa44d3365d57bfe171
Ryo Hanafusa <ryo7gumi@gmail.com>
2025-06-24 23:01:10 +0900

    feat(background): develop details of background.js

--- background/background.js
+++ background/background.js
@@ -1,79 +1,81 @@
-function makeNotification() {
+function makeNotification(title) {
+    const showingSeconds = 5;
     chrome.notifications.create(
         {
             type: 'basic',
-            iconUrl: '../images/icon48.png',
-            title: 'Automatically Closed Notification',
-            message: 'This notification will close automatically after 5 seconds.',
+            iconUrl: '../images/icon512-with-space-white-bg.png',
+            title,
+            message: `This notification will close automatically after ${showingSeconds} seconds.`,
             priority: 1,
             requireInteraction: false
         },
         (notificationId) => {
             if (chrome.runtime.lastError) {
-                console.error('Notification creation error:', chrome.runtime.lastError.message);
                 return;
+            } else {
+                setTimeout(() => {
+                    chrome.notifications.clear(notificationId);
+                }, showingSeconds * 1000);
             }
-            console.log('Notification created:', notificationId);
-
-            setTimeout(() => {
-                chrome.notifications.clear(notificationId, (wasCleared) => {
-                    if (wasCleared) {
-                        console.log('Notification closed:', notificationId);
-                    } else {
-                        console.warn('Failed to close notification:', notificationId);
-                    }
-                });
-            }, 5000); // 5000 milliseconds = 5 seconds
         }
     );
 }
 
-function logToSlack() {
-    chrome.storage.local.get('slackWebhookUrl', async (data) => {
+function zack() {
+    chrome.storage.local.get(['slackWebhookUrl', 'loggingFormatType'], async (data) => {
         const slackWebhookUrl = data.slackWebhookUrl;
         const loggingFormatType = data.loggingFormatType || 'markdown';
 
-        if (!slackWebhookUrl) {
-            // notification to user if webhook URL is not set
+        if (slackWebhookUrl) {
+            try {
+                chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+                    const activeTab = tabs[0];
+                    if (activeTab && activeTab.url) {
+                        let text = '';
+                        if (loggingFormatType === 'markdown') {
+                            text = `<${activeTab.url}|${activeTab.title}>`;
+                        } else if (loggingFormatType === 'simple') {
+                            text = `${activeTab.title}\n${activeTab.url}`;
+                        } else if (loggingFormatType === 'url-only') {
+                            text = `${activeTab.url}`;
+                        }
+
+                        const response = await fetch(slackWebhookUrl, {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify({
+                                text,
+                                unfurl_links: true,
+                            }),
+                        });
+
+                        if (response.ok) {
+                            makeNotification('Saved the current page!');
+                        } else {
+                            makeNotification('Error: Webhook request failed');
+                        }
+                    }
+                });
+            } catch (error) {
+                makeNotification('Error: Zack failed');
+            }
+        } else {
+            makeNotification('Error: No Webhook URL set');
             return;
         }
 
-        try {
-            chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
-                const activeTab = tabs[0];
-                if (activeTab && activeTab.url) {
-                    const response = await fetch(slackWebhookUrl, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({
-                            text: `<${activeTab.url}|[${new URL(activeTab.url).host}] ${activeTab.title}>`,
-                            unfurl_links: true,
-                        }),
-                    });
 
-                    if (response.ok) {
-                        // notification to user if the message was sent successfully
-                        makeNotification();
-                    } else {
-                        const errorText = await response.text();
-                        // notification to user if the message was not sent successfully
-                    }
-                }
-            });
-        } catch (error) {
-            // notification to user if there was an error sending the message
-        }
     });
 }
 
 chrome.action.onClicked.addListener(() => {
-    logToSlack();
+    zack();
 });
 
 chrome.commands.onCommand.addListener((command) => {
     if (command === '_execute_action') {
-        logToSlack();
+        zack();
     }
 });
